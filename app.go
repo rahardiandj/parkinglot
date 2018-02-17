7b0b82f26964ec02cda412e5b484345975509e67
rahardiandj <rahardiandj@gmail.com>
2018-02-17 11:42:24 +0700

    Added file reader

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/rahardiandj/parkingLot/src/parkinglot"
 )
@@ -13,24 +14,32 @@ func main() {
 	parkinglotMod := parkinglot.New(ctx)
 	parkinglotMod.Init(ctx)
 
-	car := parkinglot.CarRegistration{
-		Number: "K-02323-KK",
-		Color:  "White",
+	arg := os.Args[1]
+	if arg != "" {
+		commands := parkinglotMod.FileReader(arg)
+		if commands == nil {
+			fmt.Printf("File text is empty.\n")
+		}
 	}
-	message := parkinglotMod.CreateParkingLot(3)
-	fmt.Printf("%s\n", message.Message)
-	message = parkinglotMod.ParkCar(&car)
-	fmt.Printf("%s\n", message.Message)
 
-	message = parkinglotMod.ParkCar(&car)
-	fmt.Printf("%s\n", message.Message)
+	// car := parkinglot.CarRegistration{
+	// 	Number: "K-02323-KK",
+	// 	Color:  "White",
+	// }
+	// message := parkinglotMod.CreateParkingLot(3)
+	// fmt.Printf("%s\n", message.Message)
+	// message = parkinglotMod.ParkCar(&car)
+	// fmt.Printf("%s\n", message.Message)
 
-	message = parkinglotMod.ParkCar(&car)
-	fmt.Printf("%s\n", message.Message)
+	// message = parkinglotMod.ParkCar(&car)
+	// fmt.Printf("%s\n", message.Message)
 
-	message = parkinglotMod.LeaveLot(2)
-	fmt.Printf("%s\n", message.Message)
+	// message = parkinglotMod.ParkCar(&car)
+	// fmt.Printf("%s\n", message.Message)
 
-	message = parkinglotMod.GetStatus()
-	fmt.Printf("%s\n", message.Message)
+	// message = parkinglotMod.LeaveLot(2)
+	// fmt.Printf("%s\n", message.Message)
+
+	// message = parkinglotMod.GetStatus()
+	// fmt.Printf("%s\n", message.Message)
 }
--- src/parkinglot/parkinglot.helper.go
+++ src/parkinglot/parkinglot.helper.go
@@ -1,9 +1,38 @@
 package parkinglot
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
-func (mod *Mod) FileReader() {
+func (mod *Mod) FileReader(filePath string) []Command {
+	file, err := os.Open(filePath)
+	var commands = []Command{}
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	defer file.Close()
 
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		var command Command
+		line := fmt.Sprintf(scanner.Text())
+		strParam := strings.Fields(line)
+
+		command.CommandStr = strParam[0]
+		if len(strParam) > 2 {
+			command.Argument = strParam[1] + strParam[2]
+		}
+		if len(strParam) > 1 {
+			command.Argument = strParam[1]
+		}
+		commands = append(commands, command)
+	}
+	return commands
 }
 
 func (m *Message) Formatted(argument string) {
